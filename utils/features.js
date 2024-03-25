44e5b5d0bd9a2e5509cb7a0277331952f2fe8a55
Suvankar <suvankarmahato621@gmail.com>
2024-03-25 16:06:53 +0530

    backend done

--- utils/features.js
+++ utils/features.js
@@ -7,6 +7,8 @@ export const sendCookie=(user,res,message,statuscode=200)=>{
     res.status(statuscode).cookie("token",token,{
         httpOnly:true,
         maxAge:15*60*1000,
+        sameSite:"none",
+        secure:true
     }).json({
         success:true,
         message
