22d233292bbdf49a4be745f039f3e24b2e50dcb8
Suvankar <suvankarmahato621@gmail.com>
2024-03-25 12:19:21 +0530

    backend done

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ import express from "express";
 import userRouter from "./routes/Users.js";
 import DnsRouter from "./routes/DnsRouter.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"
+
 
 
 
 export const app=express();
 
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 
 
