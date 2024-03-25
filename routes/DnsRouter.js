f33483ff8a688e0767cb7f5f71dd63cc4f54c2d5
Suvankar <suvankarmahato621@gmail.com>
2024-03-25 15:10:27 +0530

    backend done

--- routes/DnsRouter.js
+++ routes/DnsRouter.js
@@ -4,9 +4,9 @@ import { isAuthenticated } from "../middleware/auth.js";
 
 const router=express.Router();
 
-router.get("/dns",GetAllDns);
-router.put("/dns/add", AddDns);
-router.put("/dns/edit" , UpdateDns);
-router.delete("/dns/delete", DeleteDns);
+router.get("/dns",isAuthenticated,GetAllDns);
+router.put("/dns/add", isAuthenticated, AddDns);
+router.put("/dns/edit", isAuthenticated, UpdateDns);
+router.delete("/dns/delete",isAuthenticated, DeleteDns);
 
 export default router;
